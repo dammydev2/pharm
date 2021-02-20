7a78b2d858e43ef4f73432421ef3d3588ac1166f
dammydev2 <damilola.yakubu@yahoo.com>
2021-02-20 16:33:36 +0100

    check multiple reports

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::get('/monthlyConsumption', 'HomeController@monthlyConsumption');
 Route::get('/getMonthlyConsumption', 'HomeController@getMonthlyConsumption');
 Route::get('/deptStockReport', 'HomeController@deptStockReport');
 Route::get('/getDeptStockReport', 'HomeController@getDeptStockReport');
+Route::get('/multipleMonths', 'HomeController@multipleMonths');
 Route::post('/checkStockReport', 'HomeController@checkStockReport');
 Route::post('/checkDeptStockReport', 'HomeController@checkDeptStockReport');
 Route::post('/checkMonthReport', 'HomeController@checkMonthReport');
