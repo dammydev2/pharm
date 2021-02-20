6c505633ef332ab798a2572030a53f98550ba4c2
dammydev2 <damilola.yakubu@yahoo.com>
2021-02-20 19:13:26 +0100

    single month report

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -10,9 +10,10 @@
 @if(\Auth::User()->type === 'store')
 <li><a href="{{ url('/stock') }}">Main Store/Stock</a></li>
 <li><a href="{{ url('/stockReport') }}">Stock Report</a></li>
-<li><a href="{{ url('/monthlyConsumption') }}">Monthly Consumption</a></li>
+<li><a href="{{ url('/monthlyConsumption') }}">Monthly Consumption/forecast</a></li>
 <li><a href="{{ url('/deptStockReport') }}">Stock report by department</a></li>
 <li><a href="{{ url('/multipleMonths') }}">Multiple months consuption</a></li>
+<li><a href="{{ url('/singleMonth') }}">Single month consuption</a></li>
 @endif
 
 @if(\Auth::User()->type == 'substore')
